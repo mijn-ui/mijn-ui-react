ed139e62cca2ad136534e141eb1c97b74a1a5e1b
auung <akkyaw798@gmail.com>
2024-12-02 14:36:02 +0700

    docs: fix typo in command example

--- apps/docs/examples/command/command-example.tsx
+++ apps/docs/examples/command/command-example.tsx
@@ -43,7 +43,7 @@ const CommandExample = () => {
           </CommandItem>
           <CommandItem disabled>
             <LuCalculator className="mr-2 size-4" />
-            <span>LuCalculator</span>
+            <span>Calculator</span>
           </CommandItem>
         </CommandGroup>
         <CommandSeparator />
