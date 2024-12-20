229fd97bf83890cb709bb9b5071b8b41fc6b0644
HTLA380 <htetlin099798@gmail.com>
2024-12-20 22:27:35 +0630

    chore(packages|button): remove unnecesary code

--- packages/components/button/src/button.tsx
+++ packages/components/button/src/button.tsx
@@ -1,29 +1,18 @@
 import * as React from "react"
 import { UnstyledProps, createTVUnstyledSlots } from "@mijn-ui/react-core"
-import { cn } from "@mijn-ui/react-utilities"
 import { Slot, Slottable } from "@radix-ui/react-slot"
 import { LoaderCircleIcon } from "@mijn-ui/shared-icons"
 import { ButtonVariantProps, buttonStyles } from "@mijn-ui/react-theme"
-import { ClassValue } from "tailwind-variants"
-
-/**
- * This Typescript utility transform a list of slots into a list of {slot: classes}
- */
-export type SlotsToClasses<S extends string> = {
-  [key in S]?: ClassValue
-}
 
 export type ButtonProps = React.ComponentPropsWithRef<"button"> &
   ButtonVariantProps & {
     asChild?: boolean
     loading?: boolean
-    classNames?: SlotsToClasses<keyof ReturnType<typeof buttonStyles>>
   } & UnstyledProps
 
 const Button = ({
   unstyled,
   className,
-  classNames,
   color,
   variant,
   size,
@@ -40,13 +29,11 @@ const Button = ({
 
   return (
     <Component
-      className={base({ className: cn(classNames?.base, className) })}
+      className={base({ className })}
       disabled={loading || disabled}
       {...props}
     >
-      {loading && (
-        <LoaderCircleIcon className={icon({ className: classNames?.icon })} />
-      )}
+      {loading && <LoaderCircleIcon className={icon()} />}
       <Slottable>{loading ? "Loading..." : children}</Slottable>
     </Component>
   )
