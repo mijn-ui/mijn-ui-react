67018589472f96c0c5279c97a2b9fc504815f866
HTLA380 <htetlin099798@gmail.com>
2024-12-28 20:49:26 +0630

    chore: fix lint issues

--- apps/docs/app/components/block-viewer.tsx
+++ apps/docs/app/components/block-viewer.tsx
@@ -19,17 +19,17 @@ const BlockViewer = ({ item }: BlockerViewerProps) => {
       <div className="grid w-full gap-4">
         <ResizablePanelGroup direction="horizontal" className="relative z-10">
           <ResizablePanel
-            className="relative aspect-[4/2.5] rounded-xl border bg-main md:aspect-auto"
+            className="bg-main relative aspect-[4/2.5] rounded-xl border md:aspect-auto"
             defaultSize={100}
             minSize={30}
           >
             <iframe
               src={`/react/view/${item.name}`}
               height={item.meta?.iframeHeight ?? 930}
-              className="relative z-20 hidden w-full bg-main md:block"
+              className="bg-main relative z-20 hidden w-full md:block"
             />
           </ResizablePanel>
-          <ResizableHandle className="relative hidden w-3 bg-transparent p-0 after:absolute after:right-0 after:top-1/2 after:h-8 after:w-[6px] after:-translate-x-px after:-translate-y-1/2 after:rounded-full after:bg-main-border after:transition-all after:hover:h-10 md:block" />
+          <ResizableHandle className="after:bg-main-border relative hidden w-3 bg-transparent p-0 after:absolute after:right-0 after:top-1/2 after:h-8 after:w-[6px] after:-translate-x-px after:-translate-y-1/2 after:rounded-full after:transition-all after:hover:h-10 md:block" />
           <ResizablePanel defaultSize={0} minSize={0} />
         </ResizablePanelGroup>
       </div>
