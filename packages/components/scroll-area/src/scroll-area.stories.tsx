8bea635f9e1f71e65af43589a95ba1af4fe10fd5
auung <akkyaw798@gmail.com>
2024-12-30 14:53:15 +0700

    feat(packages|components): add stories for scroll-area component

---
+++ packages/components/scroll-area/src/scroll-area.stories.tsx
@@ -0,0 +1,54 @@
+import type { Meta, StoryObj } from "@storybook/react"
+import { ScrollArea, ScrollAreaProps } from "./scroll-area"
+import { Separator } from "@mijn-ui/react-separator"
+import React from "react"
+
+const meta: Meta<typeof ScrollArea> = {
+  title: "Components/ScrollArea",
+  component: ScrollArea,
+  parameters: {
+    layout: "centered",
+  },
+  args: {
+    unstyled: false,
+  },
+}
+
+export default meta
+type Story = StoryObj<typeof ScrollArea>
+
+const tags = Array.from({ length: 50 }).map(
+  (_, i, a) => `v0.0.1-beta.${a.length - i}`,
+)
+
+const ScrollAreaTemplate = (args: ScrollAreaProps) => {
+  return (
+    <ScrollArea
+      className="bg-surface h-72 w-48 overflow-y-auto rounded-md border"
+      {...args}
+    >
+      <div className="p-4">
+        <h4 className="mb-4 text-sm font-medium leading-none">Tags</h4>
+        {tags.map((tag) => (
+          <React.Fragment key={tag}>
+            <div key={tag} className="text-sm">
+              {tag}
+            </div>
+            <Separator className="my-2" />
+          </React.Fragment>
+        ))}
+      </div>
+    </ScrollArea>
+  )
+}
+
+export const Default: Story = {
+  render: ScrollAreaTemplate,
+}
+
+export const Unstyled: Story = {
+  render: ScrollAreaTemplate,
+  args: {
+    unstyled: true,
+  },
+}
