dd4d6a340aa78ceb561f12403343d74b50431cd2
HTLA380 <htetlin099798@gmail.com>
2025-01-16 02:40:03 +0630

    docs: update input examples

---
+++ apps/docs/examples/input/input-floating-label.tsx
@@ -0,0 +1,7 @@
+import { Input } from "@mijn-ui/react"
+
+const InputFloatingLabel = () => {
+  return <Input classNames={{ wrapper: "w-80" }} label="Username" />
+}
+
+export default InputFloatingLabel
