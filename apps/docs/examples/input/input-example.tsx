945586f3a77e3f15cd6a0b7ca11272c2dbd990c7
HTLA380 <htetlin099798@gmail.com>
2025-01-16 02:40:03 +0630

    docs: update input examples

---
+++ apps/docs/examples/input/input-example.tsx
@@ -0,0 +1,7 @@
+import { Input } from "@mijn-ui/react"
+
+const InputExample = () => {
+  return <Input classNames={{ wrapper: "w-80" }} placeholder="Username..." />
+}
+
+export default InputExample
